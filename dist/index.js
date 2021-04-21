92ef2c11c5552a4ead7e7f63767fdfb33439c048
ianfights <ianfights@gmail.com>
2021-04-21 18:07:28 -0400

    Added token, db_user, db_pass, db_name, and db_ip to env variables changed index file to start bot (may make it a seperate class later) rearranged some files added a dictonary for discord.js

--- dist/index.js
+++ dist/index.js
@@ -18,14 +18,80 @@ var __importStar = (this && this.__importStar) || function (mod) {
     __setModuleDefault(result, mod);
     return result;
 };
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.con = void 0;
+const fs = __importStar(require("fs"));
 const mysql = __importStar(require("mysql"));
-var con = mysql.createConnection({
-    host: "192.168.1.159",
-    user: "pi",
-    password: "davis",
-    database: "bot",
+const discord_js_1 = require("discord.js");
+const config_1 = require("./config/config");
+const client = new discord_js_1.Client();
+const config_json_1 = __importDefault(require("./config/config.json"));
+const PREFIX = config_json_1.default.prefix;
+const con = mysql.createConnection({
+    host: config_1.dbIp,
+    user: config_1.dbUser,
+    password: config_1.dbPass,
+    database: config_1.dbName,
     multipleStatements: false
 });
 exports.con = con;
+client.commands = new discord_js_1.Collection();
+loadCommands(client.commands, `${__dirname}/commands`);
+function loadCommands(collection, folder) {
+    const commandFiles = fs.readdirSync(folder);
+    for (const file of commandFiles) {
+        const path = `${folder}/${file}`;
+        if (file.endsWith('.js')) {
+            const command = require(path);
+            //console.log(command)
+            collection.set(command.name, command);
+        }
+        else if (fs.lstatSync(path).isDirectory()) {
+            loadCommands(collection, path);
+        }
+    }
+}
+;
+client.once('ready', () => {
+    console.log("Bot is ready");
+});
+client.on('message', (message) => __awaiter(void 0, void 0, void 0, function* () {
+    // Loop for updating the leaderboard
+    /*
+        const command = client.commands.get(commandName)
+            || client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
+    
+        if (!command) return;
+    */
+    if (message.content.startsWith(PREFIX)) {
+        const input = message.content.slice(PREFIX.length).split(' ');
+        const commandName = input.shift();
+        const commandArgs = input.join(' ');
+        const splitArgs = commandArgs.split(' ');
+        //member.roles.cache.has('713076591118516314');
+        const command = client.commands.get(commandName)
+            || client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
+        if (!command)
+            return;
+        try {
+            client.commands.get(command.name).execute(message, client, commandArgs, splitArgs);
+        }
+        catch (error) {
+            console.error(error);
+            message.reply('there was an error trying to execute that command!');
+        }
+    }
+}));
+client.login(config_1.token);
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,92 @@
-import Discord from 'discord.js';
+import * as fs from 'fs';
 import * as mysql from 'mysql';
+import { Client, Collection,} from 'discord.js'
+import {dbUser, dbPass, dbIp, token, dbName} from './config/config';
+const client = new Client();
+import config from './config/config.json';
+const PREFIX = config.prefix;
 
-var con = mysql.createConnection({
-    host: "192.168.1.159",
-    user: "pi",
-    password: "davis",
-    database: "bot",
+const con = mysql.createConnection({
+    host: dbIp,
+    user: dbUser,
+    password: dbPass,
+    database: dbName,
     multipleStatements: false
 });
+
 export {con as con}
+
+client.commands = new Collection();
+loadCommands(client.commands, `${__dirname}/commands`);
+
+function loadCommands(collection, folder) {
+    const commandFiles = fs.readdirSync(folder);
+
+    for (const file of commandFiles) {
+        const path = `${folder}/${file}`;
+        if (file.endsWith('.js')) {
+            const command = require(path);
+            //console.log(command)
+            collection.set(command.name, command);
+        }
+        else if (fs.lstatSync(path).isDirectory()) {
+            loadCommands(collection, path);
+        }
+    }
+};
+
+
+
+
+
+
+client.once('ready', () => {
+    console.log("Bot is ready");
+});
+
+
+
+
+client.on('message', async message => {
+    // Loop for updating the leaderboard
+
+
+    /*
+        const command = client.commands.get(commandName)
+            || client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
+    
+        if (!command) return;
+    */
+
+    if (message.content.startsWith(PREFIX)) {
+
+        const input = message.content.slice(PREFIX.length).split(' ');
+        const commandName = input.shift();
+        const commandArgs = input.join(' ');
+        const splitArgs = commandArgs.split(' ');
+
+
+        //member.roles.cache.has('713076591118516314');
+        const command = client.commands.get(commandName)
+            || client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
+
+        if (!command) return;
+
+
+        try {
+            client.commands.get(command.name).execute(message, client, commandArgs, splitArgs);
+        } catch (error) {
+            console.error(error);
+            message.reply('there was an error trying to execute that command!');
+        }
+
+
+    }
+
+
+})
+client.login(token);
+
+
+
+
